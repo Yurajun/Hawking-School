5a136451c282f185f6c3301062bfbb5561b1d9bd
Yura Komarov <yk@hawkingbrothers.com>
2018-08-03 07:50:08 +0300

    added total hight to course slider

--- app/blocks/courses-slider/courses-slider.js
+++ app/blocks/courses-slider/courses-slider.js
@@ -1,14 +1,33 @@
 /* global $ */
 
-import 'slick-carousel';
+// import 'slick-carousel';
 
-$('.js-courses-slider').slick({
-	slidesToShow: 4
-	// responsive: [{
-	// 	breakpoint: 2000,
-	// 	settings: {
-	// 		slidesToShow: 4,
-	// 		infinite: true
-	// 	}
-	// }]
+// $('.js-courses-slider').slick({
+// 	slidesToShow: 4
+// 	// responsive: [{
+// 	// 	breakpoint: 2000,
+// 	// 	settings: {
+// 	// 		slidesToShow: 4,
+// 	// 		infinite: true
+// 	// 	}
+// 	// }]
+// });
+
+const $courseItem = $('.js-course-item');
+
+if ($(window).width() > 767 && $(window).width() < 1280){
+	const maxHeight = Math.max.apply(null, Array.from($courseItem.map((ind, el) => $(el).outerHeight())));
+	$courseItem.css('height', maxHeight + 'px');
+}
+
+$(window).on('resize', () => {
+
+	const windowWidth = $(window).width();
+	console.log(windowWidth);
+	if (windowWidth > 767 && windowWidth < 1280){
+		const maxHeight = Math.max.apply(null, Array.from($courseItem.map((ind, el) => $(el).outerHeight())));
+		$courseItem.css('height', maxHeight + 'px');
+	}else {
+		$courseItem.css('height', '100%');
+	}
 });
--- app/blocks/preview-item/preview-item.js
+++ app/blocks/preview-item/preview-item.js
@@ -1,5 +1,10 @@
 /* global $ */
 
 const $img = $('.js-picture-img');
+$('.js-picture-img').each((le, i) => {
+	console.log($(i).width());
+}
+);
 const maxWidth = Math.max.apply(null, Array.from($img.map((ind, el) => $(el).width())));
+console.log('mw ', maxWidth);
 $img.css('width', maxWidth + 'px');
